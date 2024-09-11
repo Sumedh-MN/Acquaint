107047575ea5d15039e83e07cf8dc3faac44d63a
Sumedh-MN <151665079+Sumedh-MN@users.noreply.github.com>
2024-09-11 11:17:22 +0530

    Delete .vscode/Palindrome.py

--- .vscode/Palindrome.py
+++
@@ -1,9 +0,0 @@
-text = str(input("Write some text "))
-no = int(input("Provide a number "))
-reverse_text = text[::-1]
-reverse_no = 0
-reverse_no = reverse_no*10 + no%10
-no//=10
-if (reverse_text == text and str(reverse_no == no)):
- print("The given text " + text + " and number " + str(no) + " is palindrome ")
- 
