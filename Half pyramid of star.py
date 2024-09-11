c45791e6dc2040c743dd1ab07b4eb08d2c59f861
Sumedh-MN <smncet230@gmail.com>
2024-09-11 10:30:21 +0530

    commit

--- Half pyramid of star.py
+++ Half pyramid of star.py
@@ -5,3 +5,4 @@ for i in range(0,n):
     print()
         
 
+
---
+++ Square & cube of no.py
@@ -0,0 +1,7 @@
+n= int(input())
+square=0 
+cube=0
+square=n*n
+cube = n**3
+print("square of " + str(n) + " is" ,square)
+print("cube of " + str(n) + " is" ,cube)
