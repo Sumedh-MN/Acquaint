183b21aa16eed449dcd1b9c5a8bb2f9b50a2cbf3
Sumedh-MN <smncet230@gmail.com>
2024-09-11 12:30:02 +0530

    commit

---
+++ Simple arithmetic.py
@@ -0,0 +1,10 @@
+import math
+n1=int(input("enter number 1 "))
+n2=int(input("enter number 2 "))
+sum=product=diff=div = 0
+sum = n1 + n2
+product = n1*n2
+diff = n1-n2
+div = n1/n2
+print("sum is ",sum,", product is ",product, ", difference is ",diff, ", quotient is ",div)
+print("factorial of "+ str(n1) + " is " ,math.factorial(n1))
